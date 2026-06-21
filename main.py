48da9ca2a672f51e96cd70daee8b052cee048300
LukewarmTAS <107953014+Fishikaze@users.noreply.github.com>
2026-06-21 10:58:43 -0400

    Second

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import fastapi, pydantic, uvicorn
-
-
+from supabase import create_client
+ 
+SUPABASE_URL = "https://ptjzlkfopudoowqygbtw.supabase.co"
+SUPABASE_KEY = "sb_publishable_bUCQLgx2FLX4o3TxkRnoJQ_wdD6gsGj"
+ 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+ 
 app = FastAPI()
-
-
+ 
+ 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {
-        "item_id": item_id,
-        "query": q
-    }
+def root():
+    return {"status": "online"}
+ 
+ 
+@app.get("/users")
+def get_users():
+    response = supabase.table("users").select("*").execute()
+    return response.data
